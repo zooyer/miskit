89f1135953883af35d3fe53929b630e0d9ad3fb5
zzy <zhangzhongyuan@genkitol.com>
2022-05-20 20:13:25 +0800

    修复响应函数panic捕获

--- micro/controller.go
+++ micro/controller.go
@@ -75,6 +75,12 @@ func (c Controller) Bind(ctx *gin.Context, v interface{}) (err error) {
 }
 
 func (c Controller) Response(ctx *gin.Context, data interface{}, err error) {
+	if e := recover(); e != nil {
+		if err == nil {
+			err = errors.New(errors.ServicePanic, fmt.Errorf("%s", e))
+		}
+	}
+
 	if err != nil {
 		errno := errors.New(errors.UnknownError, err)
 		errno.Record(ctx).Metric()
