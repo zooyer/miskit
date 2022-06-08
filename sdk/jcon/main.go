c45c6cf9deab4d95739f05397f58b1aaa49ac04b
zzy <zhangzhongyuan@genkitol.com>
2022-06-08 16:17:50 +0800

    增加jcon

---
+++ sdk/jcon/main.go
@@ -0,0 +1,60 @@
+package jcon
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"path/filepath"
+
+	"github.com/zooyer/jsons"
+)
+
+func Init(path string) error {
+	files, err := ioutil.ReadDir(path)
+	if err != nil {
+		return err
+	}
+	var c = make(map[string]map[string]*cacheInfo)
+	for _, file := range files {
+		if !file.IsDir() {
+			continue
+		}
+		ns, err := readNamespace(filepath.Join(path, file.Name()))
+		if err != nil {
+			return err
+		}
+		c[file.Name()] = ns
+	}
+	cache = c
+	dir = path
+	return nil
+}
+
+func Notify(namespace, name string, fn func()) (err error) {
+	return notify(namespace, name, fn)
+}
+
+func GetData(namespace, name string) (data []byte, err error) {
+	return getData(namespace, name)
+}
+
+func GetConfig(namespace, name string, v interface{}) (err error) {
+	data, err := getData(namespace, name)
+	if err != nil {
+		return
+	}
+
+	var raw []jsons.Raw
+	if err = json.Unmarshal(data, &raw); err != nil {
+		return
+	}
+
+	for i, r := range raw {
+		raw[i] = r.Get("config")
+	}
+
+	if data, err = json.Marshal(raw); err != nil {
+		return
+	}
+
+	return json.Unmarshal(data, v)
+}
---
+++ sdk/jcon/main_test.go
@@ -0,0 +1,49 @@
+package jcon
+
+import (
+	"fmt"
+	"testing"
+	"time"
+
+	"github.com/zooyer/jsons"
+)
+
+func init() {
+	var err error
+	if err = Init("./test"); err != nil {
+		panic(err)
+	}
+}
+
+func TestGetConfig(t *testing.T) {
+	var err error
+	var raw jsons.Raw
+	if err = GetConfig("test", "test", &raw); err != nil {
+		t.Fatal(err)
+	}
+
+	t.Log(string(raw))
+
+	for i := 0; i < 60; i++ {
+		var raw []byte
+		if raw, err = GetData("test", "abc"); err != nil {
+			t.Fatal(err)
+		}
+		fmt.Println(string(raw))
+		time.Sleep(time.Second)
+	}
+}
+
+func TestNotify(t *testing.T) {
+	var err error
+	fn := func() {
+		data, err := GetData("test", "def")
+		if err != nil {
+			fmt.Println(err.Error())
+		}
+		fmt.Println(string(data))
+	}
+	if err = Notify("test", "def", fn); err != nil {
+		t.Fatal(err)
+	}
+}
