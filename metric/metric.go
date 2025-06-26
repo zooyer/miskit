e05bc020dbcab7e206392c6f59b2c79db78ea453
zzy <zhangzhongyuan132@163.com>
2025-06-26 13:36:59 +0800

    优化metric

--- metric/metric.go
+++ metric/metric.go
@@ -5,10 +5,20 @@ import (
 	"time"
 )
 
+var debug bool
+
 func Rpc(name, caller, callee string, code int, latency time.Duration, tag map[string]interface{}) {
-	fmt.Println("[METRIC - RPC]", time.Now(), name, caller, callee, code, latency, tag)
+	if debug {
+		fmt.Println("[METRIC - RPC]", time.Now(), name, caller, callee, code, latency, tag)
+	}
 }
 
 func Count(name string, count int, tag map[string]interface{}) {
-	fmt.Println("[METRIC - COUNT]", time.Now(), name, count, tag)
+	if debug {
+		fmt.Println("[METRIC - COUNT]", time.Now(), name, count, tag)
+	}
+}
+
+func SetDebug(dbg bool) {
+	debug = dbg
 }
